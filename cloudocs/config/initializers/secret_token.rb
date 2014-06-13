97ff678159fd4eb7b3e25655e052fcb6a59f3c4a
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-06-13 17:25:28 +0900

    create rails project

---
+++ cloudocs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cloudocs::Application.config.secret_key_base = 'f89a3bf4be85ed108fda38f788a75a379b15e8691591d25b501b17899d7eaf3387b15cf24a8fe8c4c59e063da4e40e863cae4089bedbf9daa6e193d1d1fc4d5f'

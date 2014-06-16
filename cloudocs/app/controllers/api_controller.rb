beb15dc0e0c917495975761c599c76b1d4bda8f2
Takuya Okamoto <characterokamo@gmail.com>
2014-06-17 00:22:23 +0900

    webhook test commit

--- cloudocs/app/controllers/api_controller.rb
+++ cloudocs/app/controllers/api_controller.rb
@@ -1,15 +1,32 @@
 require 'json'
 
 class ApiController < ActionController::Base
-  def payload    
+  def payload
+    is_master = 1
+    is_pusher = 0
     if request.post?
-      push = JSON.parse(params[:payload], quirks_mode: true)
-      puts "I got some JSON"
-    else
-    end  
+      #push = params[:api] ? JSON.parse(params[:api]) : nil
+      params.each do |key, value|
+        if key == "ref"
+          if value == "/ref/heads/master"
+            is_master = 1
+          end
+        end
+        if key == "pusher"
+          is_pusher = 1
+        end
+      end
+    end
+    if is_pusher == 1 && is_master == 1
+      #pull 
+      p "We gonna pull!!!"
+      result = system("pull")
+    end
   end
 
   def get_file
   end
+
+  
   protect_from_forgery with: :null_session
 end

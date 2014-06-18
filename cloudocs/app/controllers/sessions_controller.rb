aee612537494cb0227b3df681faf33432078991f
Yamabe Yoshiro <yamab446@gmail.com>
2014-06-19 00:33:11 +0900

    login時の問題をたぶん解決

--- cloudocs/app/controllers/sessions_controller.rb
+++ cloudocs/app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 	user = User.find_by_name params[:name]
 	if user && user.authenticate(params[:password])
 		session[:user_id] = user.id
-		redirect_to session_path
+		redirect_to root_path
 	else
       		flash.now.alert = "もう一度やり直してんなー"
       		render "new"
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     @current_user = nil
-    redirect_to session_path
+    redirect_to root_path
 
   end
 

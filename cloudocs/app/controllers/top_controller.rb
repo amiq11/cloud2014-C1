263d5c08ace4ce2fd36b78406d6fa6eeb3618f18
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-06-20 19:23:36 +0900

    add comma

--- cloudocs/app/controllers/top_controller.rb
+++ cloudocs/app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 skip_before_filter :login_required
  def index
    if @current_user 
-     redirect_to :controller => "editor" :action => "index"
+     redirect_to :controller => "editor", :action => "index"
    end
  end
 end

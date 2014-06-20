5e0327a16a0806fec767c9c4b323e8cc451e2e7e
Hiroki Yamamoto <yamahiro6843@gmail.com>
2014-06-20 19:19:30 +0900

    TOP PAGE redirect to INDEX ( LOGIN STATUS

--- cloudocs/app/controllers/top_controller.rb
+++ cloudocs/app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 class TopController < ApplicationController
 skip_before_filter :login_required
  def index
+   if @current_user 
+     redirect_to :controller => "editor" :action => "index"
+   end
  end
 end
